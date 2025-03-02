2102f2a14cac0897817050a0601e0583141f1d97
rohith <rohithsai4734@gmail.com>
2025-03-02 21:23:31 +0530

    HashRouter

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter >
-
+  // <BrowserRouter >
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+    {/* </BrowserRouter> */}
+  </HashRouter>
 
 );
 
